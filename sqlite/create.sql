2b5eb51041aa08baeaad57f36dc6e4c0bfe19e04
Cosmin Deaconu <cozzyd@gmail.com>
2018-01-21 02:57:50 -0600

    small change to sql schema

--- sqlite/create.sql
+++ sqlite/create.sql
@@ -1,5 +1,5 @@
 create table hk (id integer primary key, detector integer, hk_date date, hk_time integer, processed_time datetime);  
-create table startup (id integer primary key, detector integer, hk_date date, hk_time integer, processed_time datetime);  
+create table startup (id integer primary key, detector integer, name char(64), processed_time datetime);  
 create table status (id integer primary key, run integer, detector integer, filename integer, bytes integer, processed_time datetime) ;
 create table header (id integer primary key, run integer, detector integer, filename integer, bytes integer, processed_time datetime) ;
 create table event  (id integer primary key, run integer, detector integer, filename integer, bytes integer, processed_time datetime) ;
