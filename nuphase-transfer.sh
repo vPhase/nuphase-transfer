83db5ed651b240adcf496d7074db14510d4e3647
Cosmin Deaconu <cozzyd@gmail.com>
2018-01-23 01:42:25 -0600

    set up ld_library_path

--- nuphase-transfer.sh
+++ nuphase-transfer.sh
@@ -2,6 +2,7 @@
 
 export PATH=$PATH:`pwd`/cpp 
 export NUPHASE_DATABASE=`pwd`/nuphase-transfer.db 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/../libnuphase 
 
 echo "-------  `date ` ------------" >> nuphase-transfer.log; 
 echo "-------  `date ` ------------" >> nuphase-transfer.err; 
