1b8e0e4dbe40a6223e4c4fcf79529e89bd006d26
Cosmin Deaconu <cozzyd@gmail.com>
2018-01-23 02:06:38 -0600

    execute file directly

--- nuphase-transfer.sh
+++ nuphase-transfer.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/../libnuphase
 
 echo "-------  `date ` ------------" >> nuphase-transfer.log; 
 echo "-------  `date ` ------------" >> nuphase-transfer.err; 
-nohup python python/manager.py >> nuphase-transfer.log 2>> nuphase-transfer.err < /dev/null  &
+nohup python/manager.py >> nuphase-transfer.log 2>> nuphase-transfer.err < /dev/null  &
 
 echo "Started monitor program." 
