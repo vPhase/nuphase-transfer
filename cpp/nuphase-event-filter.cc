b8f76d3d05150a3d8e5be4f8d10b0ca327c27e2c
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-03-23 14:48:45 -0500

    actually commit the source code...

--- cpp/nuphase-event-filter.cc
+++ cpp/nuphase-event-filter.cc
@@ -1,7 +1,7 @@
 
 /** This program takes in an event and header file, and will produced a
  * filtered output event file, which will contain the specified number of
- * random events, sw trigger events, and best RF events 
+ * random events, sw trigger events, and calibration  RF events 
  *
  * right now, it's very dumb and will require holding all events in memory. This could be fixed if it's a problem. 
  */ 
@@ -14,25 +14,16 @@
 #include <vector> 
 
 
-static bool compare_headers (const std::pair<nuphase_header_t, nuphase_event_t> & a, const std::pair<nuphase_header_t, nuphase_event_t> & b)
+static bool sort_events (const nuphase_event_t & a, const nuphase_event_t & b)
 {
-  
-  uint32_t max_a = 0; 
-  uint32_t max_b = 0; 
-
-  for ( int i = 0; i < NP_NUM_BEAMS; i++)
-  {
-    if (a.first.triggered_beams & (1 << i) && a.first.beam_power[i] > max_a) max_a = a.first.beam_power[i]; 
-    if (b.first.triggered_beams & (1 << i) && b.first.beam_power[i] > max_a) max_b = b.first.beam_power[i]; 
-  }
-  return max_a > max_b; 
+  return a.event_number < b.event_number; 
 }
 
 int main(int nargs, char ** args) 
 {
   if (nargs < 7)
   {
-    fprintf(stderr,"Usage: nuphase-event_filter event_file.event.gz header_file.header.gz output.filtered.gz N_BEST N_RANDOM_RF N_RANDOM_SW\n"); 
+    fprintf(stderr,"Usage: nuphase-event_filter event_file.event.gz header_file.header.gz output.filtered.gz N_RANDOM_CALIB N_RANDOM_RF N_RANDOM_SW\n"); 
     return 1; 
   }
 
@@ -40,12 +31,13 @@ int main(int nargs, char ** args)
   gzFile in_hd_file = gzopen(args[2],"r"); 
   gzFile out_ev_file = gzopen(args[3],"wx9");   
 
-  int n_best = atoi(args[4]); 
-  int n_random = atoi(args[5]); 
+  int n_calib = atoi(args[4]); 
+  int n_rf = atoi(args[5]); 
   int n_sw = atoi(args[6]); 
 
-  std::vector<std::pair<nuphase_header_t, nuphase_event_t> > rf_events; 
-  std::vector<std::pair<nuphase_header_t, nuphase_event_t> > sw_events; 
+  std::vector<nuphase_event_t> rf_events; 
+  std::vector<nuphase_event_t> sw_events; 
+  std::vector<nuphase_event_t> calib_events; 
 
   nuphase_header_t hd; 
   nuphase_event_t ev; 
@@ -60,49 +52,52 @@ int main(int nargs, char ** args)
 
     if (hd.trig_type == NP_TRIG_RF)
     {
-      rf_events.push_back(std::pair<nuphase_header_t, nuphase_event_t>(hd,ev));
+      if(hd.gate_flag) 
+      {
+        calib_events.push_back(ev);
+      }
+      else
+      {
+        rf_events.push_back(ev);
+      }
     }
     else
     {
-      sw_events.push_back(std::pair<nuphase_header_t, nuphase_event_t>(hd,ev));
+      sw_events.push_back(ev);
     }
   }
 
   gzclose(in_ev_file); 
   gzclose(in_hd_file); 
 
-  std::sort(rf_events.begin(), rf_events.end(), compare_headers); 
-  //write out the n best (if they exist) 
-  for (int i = 0; i < std::min(n_best,(int)rf_events.size()); i++)
-  {
-    nuphase_event_gzwrite(out_ev_file, &rf_events[i].second);
-  }
-
 
   //seed rng
   std::srand(time(0)); 
 
-  //shuffle the rest: 
-  if (n_random && rf_events.size() > n_best)
-  {
-    std::random_shuffle(rf_events.begin() + n_best, rf_events.end());
-    for (int i = 0; i < std::min(n_random, (int) rf_events.size() - n_best); i++)
-    {
-      nuphase_event_gzwrite(out_ev_file, &rf_events[n_best+i].second);
-    }
-  }
+  //shuffle the events 
+  std::random_shuffle(sw_events.begin(), sw_events.end()); 
+  std::random_shuffle(rf_events.begin(), rf_events.end()); 
+  std::random_shuffle(calib_events.begin(), calib_events.end()); 
 
-  if (n_sw && sw_events.size())
-  {
-    std::random_shuffle(sw_events.begin(), sw_events.end()); 
-    for (int i = 0; i < std::min(n_sw, (int) sw_events.size()); i++)
-    {
-      nuphase_event_gzwrite(out_ev_file, &sw_events[i].second); 
-    }
-  }
+  //truncate to the right number
+  if (sw_events.size() > n_sw) sw_events.resize(n_sw); 
+  if (calib_events.size() > n_calib) calib_events.resize(n_calib); 
+  if (rf_events.size() > n_rf) rf_events.resize(n_rf); 
+
+  //combine into one vector
+  std::vector<nuphase_event_t> to_write; 
+  to_write.insert(to_write.end(), sw_events.begin(), sw_events.end()); 
+  to_write.insert(to_write.end(), rf_events.begin(), rf_events.end()); 
+  to_write.insert(to_write.end(), calib_events.begin(), calib_events.end()); 
+
+  std::sort(to_write.begin(), to_write.end(), sort_events); 
+
+  //write them out, calib, rf then sw 
+  for (unsigned i = 0; i < to_write.size(); i++) nuphase_event_gzwrite(out_ev_file, &to_write[i]); 
 
   gzclose(out_ev_file); 
   return 0; 
+
 } 
 
 
